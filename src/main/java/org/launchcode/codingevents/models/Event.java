44f0c56d23b0aff93ad8889d36ef7b551dedacb1
Carl <cindymadewhat@gmail.com>
2022-10-10 18:16:21 -0500

    eeeee

---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,115 @@
+package org.launchcode.codingevents.models;
+
+import javax.validation.constraints.*;
+import java.util.Objects;
+
+/**
+ * Created by Chris Bay
+ */
+public class Event {
+
+    private int id;
+    private static int nextId = 1;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
+    private String name;
+
+    @Size(max = 500, message = "Description too long!")
+    private String description;
+
+    @NotBlank(message = "Location is required.")
+    private String location;
+
+    @AssertTrue(message = "Registration is required.")
+    private boolean mustRegister;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    @Positive(message = "At lease one attendee required.")
+    private int numberOfAttendees;
+
+    public Event(String name, String description, String contactEmail) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+    }
+
+    public Event() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(){
+        this.location = location;
+    }
+
+    public boolean getMustRegister(){
+        return mustRegister;
+    }
+
+    public void setMustRegister(boolean mustRegister){
+        this.mustRegister = mustRegister;
+    }
+
+    public int getNumberOfAttendees() {
+        return numberOfAttendees;
+    }
+
+    public void setNumberOfAttendees(int numberOfAttendees) {
+        this.numberOfAttendees = numberOfAttendees;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
